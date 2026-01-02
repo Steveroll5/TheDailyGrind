18e81e2828794fe48ef7e2a28821d6915cca7920
Mohit <mohitghanta346@gmail.com>
2026-01-02 10:34:49 +0000

    change credits to "Valles"

--- src/app/components/violation-modal.tsx
+++ src/app/components/violation-modal.tsx
@@ -17,7 +17,7 @@ const ViolationModal = ({ onClose }: ViolationModalProps) => {
         <div className="text-center font-code">
           <h2 className="text-3xl font-bold mb-4">HR VIOLATION TICKET</h2>
           <p className="text-xl mb-6">Wage Theft Detected.</p>
-          <p className="text-2xl font-bold">Penalty: 50 Credits.</p>
+          <p className="text-2xl font-bold">Penalty: 50 Valles.</p>
         </div>
         <button
           onClick={onClose}
