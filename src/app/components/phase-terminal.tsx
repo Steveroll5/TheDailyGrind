d2bdcbd7b6d191f6ee6460595eebc65012d4abb2
Mohit <mohitghanta346@gmail.com>
2026-01-03 11:08:00 +0000

    change this "Urgent Password for lunch room: cold fries" -> "Important

--- src/app/components/phase-terminal.tsx
+++ src/app/components/phase-terminal.tsx
@@ -83,10 +83,9 @@ const PhaseTerminal = ({ onLockdown }: PhaseTerminalProps) => {
                 
                 <div className="relative">
                     <div className="bg-amber-100 text-black p-6 transform -rotate-3 shadow-lg font-handwritten">
-                        <h4 className="font-bold text-xl border-b border-black/20 pb-1 mb-2">URGENT</h4>
+                        <h4 className="font-bold text-xl border-b border-black/20 pb-1 mb-2">Important</h4>
                         <p className="text-lg">Password for lunch room:</p>
-                        <p className="font-bold text-2xl my-2">COLD FRIES</p>
-                        <p className="text-lg">(DON'T FORGET AGAIN BOB)</p>
+                        <p className="font-bold text-2xl my-2">123#</p>
                     </div>
                 </div>
             </div>
